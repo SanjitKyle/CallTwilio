63fcfe9bed3df6a00c77f59cadc82ba955cead4d
kylegit74 <kylefront74@gmail.com>
2025-11-17 14:10:18 +0530

    res

--- src/controller/CallController.js
+++ src/controller/CallController.js
@@ -1,24 +1,21 @@
 import twilio from "twilio";
 import dotenv from "dotenv";
 dotenv.config();
- 
+
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
- 
 const client = twilio(accountSid, authToken);
- 
-export  const createCall=async()=> {
+
+export const createCall = async (req, res) => {
   try {
     const call = await client.calls.create({
-      from: "+12058501038", 
-      to: "+918392056130", 
-      url: "https://calltwilio-2.onrender.com/voice", 
+      from: "+12058501038",
+      to: "+918392056130",
+      url: "https://calltwilio-2.onrender.com/voice",
     });
- 
-    console.log("Call SID:", call.sid);
+
+  
   } catch (error) {
-    console.error("Error making call:", error);
+    res.send("Error: " + error.message);
   }
-}
- 
-createCall()
+};
--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
-import express from 'express'
-import { GatherResponse, voice } from './controller/GatheRoute.js';
+import express from "express";
+import { voice, GatherResponse } from "./controller/GatheRoute.js";
+import { createCall } from "./controller/CallController.js";
 
-const App=express();
+const App = express();
 
+// Middleware for Twilio POST Body
 App.use(express.urlencoded({ extended: false }));
 App.use(express.json());
 
+// Voice Routes
 App.post("/voice", voice);
 App.post("/gather", GatherResponse);
 
+// CALL TRIGGER ROUTE
+createCall()
 
-App.listen(3000,()=>{
-  console.log('server is running at port',3000)
-})
+App.listen(3000, () => {
+  console.log("server is running at port 3000");
+});
